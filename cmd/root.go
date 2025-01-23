10fb2eccaacfc24fcbba52bc1c63cb0814ad6819
xieyongbo <xieyongbo@ideal.sh.cn>
2025-01-23 17:37:43 +0800

    add command

--- cmd/root.go
+++ cmd/root.go
@@ -1,30 +1,27 @@
 /*
 Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-
 */
 package cmd
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
 )
 
-
-
 // rootCmd represents the base command when called without any subcommands
 var rootCmd = &cobra.Command{
 	Use:   "auto",
-	Short: "A brief description of your application",
-	Long: `A longer description that spans multiple lines and likely contains
-examples and usage of using your application. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "1. root",
+	Long:  `root is the root command of the application.`,
 	// Uncomment the following line if your bare application
 	// has an action associated with it:
 	// Run: func(cmd *cobra.Command, args []string) { },
+	Args: cobra.ExactArgs(1),
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("Create rule %s success.\n", args[0])
+	},
 }
 
 // Execute adds all child commands to the root command and sets flags appropriately.
@@ -46,6 +43,7 @@ func init() {
 	// Cobra also supports local flags, which will only run
 	// when this action is called directly.
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	// 在指定标志时可以用 --name，也可以使用短写 -n
+	rootCmd.Flags().StringVarP(&name, "name", "n", "", "createCmd下的name参数")
+
 }
-
-
